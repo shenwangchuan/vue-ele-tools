4c323be1a447f493b26ff37c60813f6a62ec616f
shenwangchuan <yisu512@sina.com>
2019-05-10 17:41:52 +0800

    数据表单组件开发

---
+++ src/components/gridForm/GridForm.vue
@@ -0,0 +1,71 @@
+<!--数据表单 -->
+<template>
+  <el-form class="GridForm" :model="form" inline :size="size" :disabled="disabled">
+    <el-form-item v-for="item in data"
+                  :key="item.name"
+                  :label="item.label"
+                  :label-width="item.labelWidth"
+                  :prop="item.name"
+                  :size="item.size"
+                  :rules="item.rules"
+    >
+      <!--输入框-->
+      <el-input v-if="item.type==='input'"
+                :type="item.inputType"
+                v-model="form[item.name]"
+                :placeholder="item.placeholder"
+                :readonly="item.readonly"
+                :disabled="item.disabled"
+                :clearable="item.clearable"
+                :show-password="item.showPassword"
+                :autosize="item.autosize"
+                :minlength="item.minlength"
+                :maxlength="item.maxlength"
+                :rows="item.rows"
+                :show-word-limit="item.showWordLimit"
+      />
+      <!--下拉框-->
+      <el-select v-if="item.type==='select'"
+                 v-model="form[item.name]"
+                 :placeholder="item.placeholder">
+        <el-option
+          v-for="opt in item.options"
+          :key="item.props ? opt[item.props.value] : opt[props.value]"
+          :label="item.props ? opt[item.props.value] : opt[props.value]"
+          :value="item.props ? opt[item.props.value] : opt[props.value]">
+        </el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script type="es6">
+  export default {
+    name: "GridForm",
+    props: {
+      data: {
+        type: Array,
+        default() { return [] }
+      },
+      size: String,
+      disabled: Boolean
+    },
+    data() {
+      return {
+        form: {},
+        props:{
+          value:'value',
+          label:'label'
+        }
+      }
+    },
+    created() {
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped lang="scss" type="text/scss">
+  .GridForm {
+  }
+</style>
---
+++ src/pages/form/dataForm.vue
@@ -0,0 +1,28 @@
+<!--表单 -->
+<template>
+  <div class="dataForm">
+
+  </div>
+</template>
+
+<script type="es6">
+  export default {
+    name: "dataForm",
+    data() {
+      return {
+        formData:[]
+      }
+    },
+    created() {
+      this.formData = [
+        {type:'input',name:'name',label:'姓名',value:'1' }
+      ]
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped lang="scss" type="text/scss">
+  .dataForm {
+  }
+</style>
